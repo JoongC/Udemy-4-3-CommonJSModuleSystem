3fffe7e77d980febd4895e02cb67586f38399fa7
JoongC <junability@gmail.com>
2022-05-17 17:39:55 -0700

    May17

---
+++ cal.js
@@ -0,0 +1,10 @@
+//calculation file
+
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b;
+
+module.exports = {
+  moduleName: "cal module",
+  add: add,
+  sub: sub,
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+const cal = require("./cal");
+
+console.log(cal.add(1, 2));
+console.log(cal.add(4, 5));
+console.log(cal.sub(10, 2));
